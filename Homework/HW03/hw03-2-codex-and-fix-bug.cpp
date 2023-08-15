6a39879dffb150a00a7bfce3baf67d0a82fa58b6
SuperNutboy <natthapong_ta66@live.rmutl.ac.th>
2023-08-15 14:11:47 +0700

    Update hw 32

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -1,24 +1,29 @@
 //TODO: #9 Fix bug and arrange code in CODEX pattern.
-#include <stdio.h>
+int main() {
+    float a;
+    float b = 3.0;
+    int i = 0;
+    int _1 = 0;
 
-int main(){
-float a,b=3;
-bool _1=0;
-int i=0;
+    printf("%d ", _1);
 
-printf(" %d ",_1);
-if(_1!=0) {
-    printf(" ok ");
-}   else {
-        while(i<=5) {
-            printf(" not-okey-%d ",++i);
+    if (_1 != 0) {
+        printf(" ok ");
+    } else {
+        while (i <= 5) {
+            printf(" not-okay-%d ", ++i);
         }
     }
-for(i=3;i<10;i+=1) {
-    if(i%2==(!1?1:2)) {
-        printf(" see see ");
+
+    for (i = 3; i < 10; i += 1) {
+        if (i % 2 == (_1 ? 1 : 0)) {
+            printf(" see see ");
+        }
     }
-    printf(" haha ");
+
+    printf("haha\n");
+	
+    return 0;
 }
-return 0;
-}
+
+
