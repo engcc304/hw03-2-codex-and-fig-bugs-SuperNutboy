67634b04ddcd983b3d73e68a94ff6a28978ba474
SuperNutboy <natthapong_ta66@live.rmutl.ac.th>
2023-08-15 14:37:36 +0700

    updatettttt

--- Homework/HW03/hw03-2-codex-and-fix-bug.cpp
+++ Homework/HW03/hw03-2-codex-and-fix-bug.cpp
@@ -8,24 +8,21 @@ int main() {
     int _1 = 0;
 
     printf("%d ", _1);
-
     if (_1 != 0) {
-        printf(" ok ");
+        printf("ok");
     } else {
         while (i <= 5) {
-            printf(" not-okay-%d ", ++i);
+            printf("not-okay-%d", ++i);
         }
     }
 
     for (i = 3; i < 10; i += 1) {
-        if (i % 2 == (_1 ? 1 : 0)) {
-            printf(" see see ");
+        if (i % 2 == (_1 ? 0 : 1)) {
+            printf("see see");
         }
     }
 
     printf("haha\n");
-	
+
     return 0;
 }
-
-
